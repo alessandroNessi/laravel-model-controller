05b57d43ef15672cad6c138cb64fb0c24409c020
alessandroNessi <alessandronessi@msn.com>
2021-11-08 16:37:06 +0100

    esercizio strutturato con sass funzionante

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         @foreach ($movies as $movie)
             {{-- @dd($movie); --}}
-            <p>{{$movie['title']}}</p>
+            {{-- <p>{{$movie['title']}}</p> --}}
             <div class="card">
                 <h2>titolo: {{$movie['title']}}</h2>
                 <h2>titolo originale: {{$movie['original_title']}}</h2>
